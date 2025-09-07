f3a8e00b5d62f3414c89e4a4b110044022188c98
Mike Adler Brignol <brignol.com@gmail.com>
2025-09-07 18:05:56 +0000

    Completed Exercise 2 to 5

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out double grade))
+        {
+            char letterGrade;
+
+            if (grade >= 90)
+                letterGrade = 'A';
+            else if (grade >= 80)
+                letterGrade = 'B';
+            else if (grade >= 70)
+                letterGrade = 'C';
+            else if (grade >= 60)
+                letterGrade = 'D';
+            else
+                letterGrade = 'F';
+
+            Console.WriteLine($"Your letter grade is: {letterGrade}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input, Please enter a number.");
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int maicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != maicNumber)
+        {
+            Console.Write("what is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (maicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (maicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You Guessed it");
+            }
+        }
     }
 }
